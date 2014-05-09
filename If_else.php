6d4a6a19933481e9de11514178af12a393b45323
Sweetrocker22 <Home@Caitlin-Mac.local>
2014-05-09 14:18:34 -0500

    Changes to formatting

--- If_else.php
+++ If_else.php
@@ -1,22 +1,22 @@
 <?php
 
 
-$age = 22;
-$is_adult = $age >= 18 ? true : false;
+// $age = 22;
+// $is_adult = $age >= 18 ? true : false;
 
-//[ condition ] ? [ value if true ] : [ value if false ];
-// So this line of code is asking: Is the age asked greater than 
-// 	or equal to 18. Tell me true or false.
+// //[ condition ] ? [ value if true ] : [ value if false ];
+// // So this line of code is asking: Is the age asked greater than 
+// // 	or equal to 18. Tell me true or false.
 
-$can_see_die_hard = ($age >= 18) ? "yes!" : "no, try Disney";
-//This is short hand for if/else statements. This is equal to the next line of code. --->
+// $can_see_die_hard = ($age >= 18) ? "yes!" : "no, try Disney";
+// //This is short hand for if/else statements. This is equal to the next line of code. --->
 
-if ($age >= 18) {
-	$can_see_die_hard = "yes!";
+// if ($age >= 18) {
+// 	$can_see_die_hard = "yes!";
 
-}	else {
-		$can_see_die_hard = "no, really, Disney";
-	}
+// }	else {
+// 		$can_see_die_hard = "no, really, Disney";
+// 	}
 
 
 
@@ -24,11 +24,11 @@ $a = 10;
 $b = '10';
 
 if ($a === $b) {
-    echo "$a is identical to $b";
-} elseif ($a == $b) {
-    echo "$a is equal to $b";
-} else {
-    echo "$a is not equal to $b";
+    echo "$a is identical to $b\n";
+}elseif ($a == $b) {
+    echo "$a is equal to $b\n";
+}else {
+    echo "$a is not equal to $b\n";
 }
 
 
@@ -40,29 +40,31 @@ $c = '10';
 // Add an else clause to the next two statements
 if ($a < $b) {
     // output the appropriate result
-    echo "$a is less than $b";
+    echo "$a is less than $b\n";
 }else { 
-    	echo "$a is greater than $b";
-};
+    	echo "$a is greater than $b\n";
+}
 
 if ($b > $a) {
     // output the appropriate result
-    echo "$b is greater than $a";
+    echo "$b is greater than $a\n";
 }else { 
-    	echo "$b is less than $a";
-};
+    	echo "$b is less than $a\n";
+}
 
 // Shorten the next 2 statements into an if/else
 if ($b >= $c) {
     // output the appropriate result
-    echo "$b is greater than $c";
+    echo "$b is greater than $c\n";
 }else{
-	echo "$b is less than $c";
-};
+	echo "$b is less than $c\n";
+}
 
 if ($b <= $c) {
     // output the appropriate result
-    echo "$b is ____ $c";
+    echo "$b is less than $c\n";
+}else {
+	echo "$b is greater than $c\n";
 }
 
 // combine the next 4 conditionals into 
@@ -70,29 +72,29 @@ if ($b <= $c) {
 // identical, equal, not identical, not equal
 if ($b == $c) {
     // output the appropriate result
-    echo "$b is equal to $c";
+    echo "$b is equal to $c\n";
 }elseif ($b === $c) {
-	echo "$b is identical to $c";	
+	echo "$b is identical to $c\n";	
 }elseif ($b != $c) {
-	echo "$b is not equal to $c";
+	echo "$b is not equal to $c\n";
 }elseif ($b !== $c) {
-	echo "$b is not identical to $c";
+	echo "$b is not identical to $c\n";
 }
 
 
 if ($b === $c) {
     // output the appropriate result
-    echo "$b is identical to $c";
+    echo "$b is identical to $c\n";
 }
 
 if ($b != $c) {
     // output the appropriate result
-    echo "$b is not equal to $c";
+    echo "$b is not equal to $c\n";
 }
 
 if ($b !== $c) {
     // output the appropriate result
-    echo "$b is not identical to $c";
+    echo "$b is not identical to $c\n";
 }
 
 ?>

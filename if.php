32798d31f7955164c655bb80e41789df68df19e4
Sweetrocker22 <Home@Caitlin-Mac.local>
2014-05-09 13:29:06 -0500

    If statements

--- if.php
+++ if.php
@@ -44,4 +44,42 @@ if ($b ===$c) {
     echo "$b is not identical to $c\n";
 }
 
+
+//Beginings of IF/ELSE statements
+
+if($a == $b) {
+    echo "$a is equal to $b\n";
+}   else {
+    echo "This statement is false.\n";
+};
+
+$won_lottery = FALSE;
+$is_raining = TRUE;
+
+if($won_lottery && $is_raining) {
+    echo "I'm buying a new car!\n";
+}   else{
+    echo "I shouldn't buy a car if I didn't win the lottery.\n";
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>

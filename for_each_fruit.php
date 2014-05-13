fc90cb7622a51d989026bbf593077f1b59e50fb5
Sweetrocker22 <Home@Caitlin-Mac.local>
2014-05-13 17:42:16 -0500

    Foreach listing of fruit and attributes.

---
+++ for_each_fruit.php
@@ -0,0 +1,21 @@
+<?php
+
+//1.Create a file called fruit.php 
+//2. Create an array of fruits. 
+//3. Write a loop that prints the fruits. for||foreach	
+//4. Modify the fruit array so that you have fruit name => color. Print using foreach. "Apples are red."
+
+$fruit = [
+	'Apples'  => [
+		'color' => red],	
+	'Bananas' => [
+		'color' => yellow],
+	'Oranges' =>[
+		'color' => orange],
+	'Kiwis'  => [
+		'color' => green]			
+];
+
+foreach ($fruit as $name => $details) {
+	echo $name . " are " . $details['color'] . PHP_EOL;
+}

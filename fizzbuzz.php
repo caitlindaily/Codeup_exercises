275b03224dc6e517d7ded563e919db92524e012d
Sweetrocker22 <Home@Caitlin-Mac.local>
2014-05-13 13:19:13 -0500

    Streamlined conditions

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -5,13 +5,16 @@
 //For both, print "FizzBuzz"
 
 for ($i = 1; $i <= 100; $i++) {
-	if ($i % 3 == 0 && $i % 5 !=0) {
-		echo "Fizz\n";	
-	} elseif ($i % 3 != 0 && $i % 5 ==0) {
-		echo "Buzz\n";
-
-	} elseif ($i % 3 ==0 && $i % 5 == 0) {
+	
+	if ($i % 3 ==0 && $i % 5 == 0) {
 		echo "FizzBuzz\n";	
+
+	} elseif ($i % 3 == 0) {
+		echo "Fizz\n";
+	
+	} elseif ($i % 5 == 0) {
+		echo "Buzz\n";	
+	
 	} else {
 		echo "{$i}\n";
 	}	

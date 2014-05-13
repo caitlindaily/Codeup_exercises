4f25f757dedfa9d23b043e3799e2e02f81a14879
Sweetrocker22 <Home@Caitlin-Mac.local>
2014-05-13 14:32:57 -0500

    Tells what the item in the array is.

--- for_each.php
+++ for_each.php
@@ -18,3 +18,19 @@ foreach ($things as $thing) {
 		echo "{$thing}\n";
 	}
 }
+//Construct a loop that iterates through each value and outputs its type as either integer, float, boolean, array, null, or string.
+foreach ($things as $thing) {
+	if (is_int($thing)){
+		echo "Integer\n";
+	}elseif (is_float($thing)) {
+		echo "Float\n";
+	}elseif (is_bool($thing)) {
+		echo "Boolean\n";
+	}elseif (is_array($thing)) {
+		echo "Array\n";
+	}elseif (is_null($thing)) {
+		echo "Null\n";
+	}elseif (is_string($thing)) {
+		echo "String\n";
+	}
+}

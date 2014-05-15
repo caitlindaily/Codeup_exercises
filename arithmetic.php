85ab64700c799417a07a2d3f467c64d2bee56cb1
Sweetrocker22 <Home@Caitlin-Mac.local>
2014-05-15 16:15:45 -0500

    Change echo to return

--- arithmetic.php
+++ arithmetic.php
@@ -1,59 +1,59 @@
 <?php
 
-function error($a, $b, $error_code = true) {
-	if ($error_code){
-		echo "ERROR: {$a} and {$b} are not numeric values.\n";
+function error($a, $b, $error_code) {
+	if ($error_code == 1){
+		return "ERROR: {$a} and {$b} are not numeric values.\n";
 	}else {
-		echo "Cannot divide by zero.\n";
+		return "Cannot divide by zero.\n";
 	}
 }
 
 function add($a, $b) {
     if (is_numeric($a) && is_numeric($b)) {
-    	echo $a + $b . PHP_EOL;
+    	return;
     }else {
-    	error($a, $b);
+    	return error($a, $b, 1);
 	}
 }
 
 function subtract($a, $b) {
    if (is_numeric($a) && is_numeric($b)) {
-    	echo $a - $b . PHP_EOL;
+    	return $a - $b . PHP_EOL;
     }else {
-    	 error($a, $b);
+    	 return error($a, $b, 1);
 	}
 }
 
 function multiply($a, $b) {
    if (is_numeric($a) && is_numeric($b)) {
-    	echo $a * $b . PHP_EOL;
+    	return $a * $b . PHP_EOL;
     }else {
-    	 error($a, $b);
+    	 return error($a, $b, 1);
 	}
 }
 function divide($a, $b) {
    if (is_numeric($a) && is_numeric($b)) {
    		if ($b == 0){
-   			error($a, $b, 0);
+   			return error($a, $b, 0);
    		}else {
-    		echo $a / $b . PHP_EOL;
+    		return $a / $b . PHP_EOL;
     	}
 	}else {
-		error($a, $b, 1);
+		return error($a, $b, 1);
 	}	
 }
 function modulus($a, $b) {
    if (is_numeric($a) && is_numeric($b)) {
-    	echo $a % $b . PHP_EOL;
+    	return $a % $b . PHP_EOL;
     }else {
-    	 error($a, $b, 1);
+    	 return error($a, $b, 1);
 	}
 }
 
-add('dog',5);
-subtract(3,5);
-multiply(2,5);
-divide(3,0);
-modulus(3,5);
+echo add('dog',5);
+echo subtract(3,5);
+echo multiply(2,5);
+echo divide(3,0);
+echo modulus(3,5);
 
 

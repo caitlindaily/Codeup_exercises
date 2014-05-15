b9486d2c71e185ac0253afb1c2801a052af7d53b
Sweetrocker22 <Home@Caitlin-Mac.local>
2014-05-15 10:02:40 -0500

    Defining user functions

---
+++ arithmetic.php
@@ -0,0 +1,34 @@
+<?php
+
+
+function add($a, $b) {
+    echo $a + $b . PHP_EOL;
+}
+
+function subtract($a, $b) {
+    // Add code here
+    echo $a - $b . PHP_EOL;
+}
+
+
+function multiply($a, $b) {
+    // Add code here
+    echo $a * $b . PHP_EOL;
+}
+
+
+function divide($a, $b) {
+    // Add code here
+    echo $a / $b . PHP_EOL;
+}
+
+
+function modulus($a, $b) {
+	echo $a % $b . PHP_EOL;
+}
+
+add(3,5);
+subtract(3,5);
+multiply(3,5);
+divide(3,5);
+modulus(3,5);

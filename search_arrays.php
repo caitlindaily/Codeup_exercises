ed1fdb44387c59286dd7bf4cf125c32ceff19dc3
Sweetrocker22 <Home@Caitlin-Mac.local>
2014-05-19 15:13:25 -0500

    Compare both arrays and return # that are same

--- search_arrays.php
+++ search_arrays.php
@@ -9,17 +9,36 @@ $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
 
 
-function find($needle, $haystack) {
-	
+function find($needle, $haystack) 
+{
 	$result = array_search($needle, $haystack);
 
-	if ($result !== FALSE) {
-		$result = True;
-	}elseif ($result == false) {
-		$result = False;
-
-	}return $result;
+	if ($result === false) {
+		return false;
+	}else {
+		return true;
+	}
 }
-var_dump(find('Tina', $names));
-var_dump(find('Bob', $names));
+// var_dump(find('Tina', $names));
+// var_dump(find('Bob', $names));
+
+
+
+function compare_array_names($array1, $array2) 
+{
+	$count = 0;
+	foreach ($array1 as $key => $value)
+	{
+		if (find($value, $array2)) {
+		 $count++;
+		}
+	}	  
+	return $count;
+}
+var_dump(compare_array_names($names, $compare));
+
+
+
+
+
 

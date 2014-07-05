f951c92748801f10b6cfb1b3128e770acb706f70
Caitlin Daily <cdaily87@gmail.com>
2014-07-05 15:43:21 -0500

    Update arithmetic.php
    Cleaned up formatting

--- arithmetic.php
+++ arithmetic.php
@@ -1,53 +1,54 @@
 <?php
 
 function error($a, $b, $error_code) {
-	if ($error_code == 1){
-		return "ERROR: {$a} and {$b} are not numeric values.\n";
-	}else {
-		return "Cannot divide by zero.\n";
-	}
+    if ($error_code == 1){
+	return "ERROR: {$a} and {$b} are not numeric values.\n";
+    } else {
+	return "Cannot divide by zero.\n";
+    }
 }
 
 function add($a, $b) {
     if (is_numeric($a) && is_numeric($b)) {
     	return;
-    }else {
+    } else {
     	return error($a, $b, 1);
-	}
+    }
 }
 
 function subtract($a, $b) {
    if (is_numeric($a) && is_numeric($b)) {
     	return $a - $b . PHP_EOL;
-    }else {
+    } else {
     	 return error($a, $b, 1);
-	}
+    }
 }
 
 function multiply($a, $b) {
    if (is_numeric($a) && is_numeric($b)) {
     	return $a * $b . PHP_EOL;
-    }else {
-    	 return error($a, $b, 1);
-	}
+    } else {
+     	return error($a, $b, 1);
+    }
 }
 function divide($a, $b) {
    if (is_numeric($a) && is_numeric($b)) {
-   		if ($b == 0){
-   			return error($a, $b, 0);
-   		}else {
+   	if ($b == 0){
+   	   return error($a, $b, 0);
+   		
+   	}else {
     		return $a / $b . PHP_EOL;
-    	}
-	}else {
-		return error($a, $b, 1);
-	}	
+    	}	
+    }else {
+	return error($a, $b, 1);
+    }	
 }
 function modulus($a, $b) {
    if (is_numeric($a) && is_numeric($b)) {
     	return $a % $b . PHP_EOL;
     }else {
-    	 return error($a, $b, 1);
-	}
+    	return error($a, $b, 1);
+    }
 }
 
 echo add('dog',5);

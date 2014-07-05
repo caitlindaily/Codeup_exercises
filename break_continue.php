47ae5fca2ddef0ffb94ef9cb7b59cf2e690123c4
Caitlin Daily <cdaily87@gmail.com>
2014-07-05 15:46:26 -0500

    Cleaned up formatting

--- break_continue.php
+++ break_continue.php
@@ -3,23 +3,23 @@
 // Create a for loop that shows all even numbers between 1 and 100 using continue.
 
 for ($i=0; $i <= 100 ; $i +=2) { 
-	if ($i % 2 == 0) {
-		echo "$i\n";
-		continue;
-	}
-}	echo "The end\n";
+    if ($i % 2 == 0) {
+	echo "$i\n";
+	continue;
+    }
+} echo "The end\n";
 
 
 
 // Create another for loop that counts from 1 to 100, but stops after 10 using break.
 
 for ($i=1; $i <= 100 ; $i++) { 
-	echo "$i\n";
-	if ($i == 10) {
-		break;
-	}
-}
-echo "The end\n";
+    echo "$i\n";
+    
+    if ($i == 10) {
+	break;
+    }
+} echo "The end\n";
 
 
 
